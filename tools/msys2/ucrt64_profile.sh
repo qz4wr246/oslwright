1a591a4c3c1e11c09f3604d46620a85028bdbd68
qz4wr246 <230674914+qz4wr246@users.noreply.github.com>
2026-04-02 14:12:11 +0900

    modify msys2

--- tools/msys2/ucrt64_profile.sh
+++ tools/msys2/ucrt64_profile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-if [ "$MSYSTEM" = "UCRT64" ]; then
-  export PKG_CONFIG_PATH="/opt/ucrt64/lib/pkgconfig:$PKG_CONFIG_PATH"
+if [[ "$MSYSTEM" == "UCRT64" ]]; then
+  export PKG_CONFIG_PATH="/opt/ucrt64/lib/pkgconfig:/opt/ucrt64/share/pkgconfig${PKG_CONFIG_PATH:+:${PKG_CONFIG_PATH}}"
   export PATH="/opt/ucrt64/bin:$PATH"
 fi
