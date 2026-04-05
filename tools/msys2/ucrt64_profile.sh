272a5f277f3584e2740b8c1af66a27fc31dc3065
qz4wr246 <230674914+qz4wr246@users.noreply.github.com>
2026-04-05 11:03:53 +0900

    typo

--- tools/msys2/ucrt64_profile.sh
+++ tools/msys2/ucrt64_profile.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 if [[ "$MSYSTEM" == "UCRT64" ]]; then
   export PKG_CONFIG_PATH="/opt/ucrt64/lib/pkgconfig:/opt/ucrt64/share/pkgconfig${PKG_CONFIG_PATH:+:${PKG_CONFIG_PATH}}"
-  export PATH="/opt/ucrt64/bin:$PATH"
+  export PATH="/opt/ucrt64/bin:/ucrt64/bin:$PATH"
+fi
+if [[ "$MSYSTEM" == "MSYS" ]]; then
+  export PKG_CONFIG_PATH="/opt/ucrt64/lib/pkgconfig:/opt/ucrt64/share/pkgconfig:/ucrt64/lib/pkgconfig:/ucrt64/share/pkgconfig${PKG_CONFIG_PATH:+:${PKG_CONFIG_PATH}}"
+  export PATH="/opt/ucrt64/bin:/ucrt64/bin:$PATH"
 fi
